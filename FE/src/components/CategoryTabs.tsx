2f8045d52404a50aa8c40434df50cc9f95006269
bakhacode <bakhamitai@naver.com>
2023-06-21 21:15:32 +0900

    feat: 카테고리탭 구현
    - UI와 슬라이딩 기능 구현완료

---
+++ FE/src/components/CategoryTabs.tsx
@@ -0,0 +1,68 @@
+import { useRef, useState } from "react";
+import styles from "./CategoryTabs.module.css";
+
+export function CategoryTabs({ categories, onClick }: { categories: any[]; onClick: (index: number) => void }) {
+  const [activeTab, setActiveTab] = useState(-1);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const handleTabClick = (index: number) => {
+    onClick(index);
+    setActiveTab(index);
+  };
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setIsDragging(true);
+    setStartX(e.clientX);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (isDragging && scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft += startX - e.clientX;
+      setStartX(e.clientX);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  return (
+    <div
+      className={styles.CategoryTabs}
+      ref={scrollContainerRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}>
+      {categories.map((category, index) => (
+        <Tab
+          key={index}
+          name={category}
+          isLastTab={index === categories.length - 1}
+          isActive={index === activeTab}
+          onClick={() => handleTabClick(index)}
+          // onClick={() => onClick(index)}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Tab({ name, isLastTab, isActive, onClick }: TabProps) {
+  return (
+    <div
+      className={`${styles.Tab} ${isLastTab ? styles.LastTab : ""} ${isActive ? styles.ActiveTab : ""}`}
+      onClick={onClick}>
+      {name}
+    </div>
+  );
+}
+
+type TabProps = {
+  name: string;
+  isLastTab: boolean;
+  isActive: boolean;
+  onClick: () => void;
+};
