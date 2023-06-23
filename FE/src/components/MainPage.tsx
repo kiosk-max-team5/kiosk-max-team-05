61cbb6be95745b05a12c48cc9b18fd9b7e476b6e
bakhacode <bakhamitai@naver.com>
2023-06-23 10:17:04 +0900

    feat: 메인 영역 구현
    - UI 구현
    - 영역 노출 시 위에서 아래로 애니메이션 효과와 함께 기존 내용은 아래로 사라진다.
    - 상품 메뉴는 판매량이 많은 기준으로 정렬(비즈니스 로직 가공된 데이터로 받음)한다
    - 가장 첫번째 메뉴에 별 태그를 붙인다.
    - 각 상품을 선택하면 '담기' 모달을 표시한다.

--- FE/src/components/MainPage.tsx
+++ FE/src/components/MainPage.tsx
@@ -1,160 +1,60 @@
 import { CategoryTabs } from "./CategoryTabs";
 import { MenuArea } from "./MenuArea";
 import styles from "./MainPage.module.css";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Cart } from "./cart/Cart";
+import ModalContext from "../ModalContext";
 
 export function MainPage() {
   const [menuData, setMenuData] = useState([]);
+  const contextValue = useContext(ModalContext);
+  if (!contextValue) {
+    throw new Error("ModalContext is not provided");
+  }
 
   const categories = ["커피", "라떼", "티", "쥬스", "디카페인"];
-  const endpoint = ["coffee", "latte", "tea", "juice", "decaf"];
+  const endpoint = useRef(["coffee", "latte", "tea", "juice", "decaf"]);
   const [currentCategory, setCurrentCategory] = useState(-1);
+  const [animationClass, setAnimationClass] = useState("fade-enter");
 
   const handleTabClick = (index: number) => {
-    setCurrentCategory(index);
+    if (index === currentCategory) return;
+    setAnimationClass("fade-enter");
+    setTimeout(() => {
+      setAnimationClass("fade-exit");
+
+      setCurrentCategory(index);
+    }, 450);
   };
 
   useEffect(() => {
     const fetchMenuData = async () => {
-      try {
-        const response = await fetch(`http://localhost:8080/${endpoint[currentCategory]}`);
-        const menuData = await response.json();
-        console.log(menuData);
-        setMenuData(menuData);
-      } catch (error) {
-        console.error("Error fetching coffee data:", error);
+      if (currentCategory >= 0) {
+        try {
+          const response = await fetch(`http://localhost:8080/${endpoint.current[currentCategory]}`);
+
+          const menuData = await response.json();
+
+          setMenuData(menuData);
+        } catch (error) {
+          console.error("Error fetching coffee data:", error);
+        }
       }
     };
-
     fetchMenuData();
-  }, [currentCategory]);
+
+    console.log("안녕");
+  }, [currentCategory, endpoint]);
+
   return (
     <div className={styles.MainPage}>
       <CategoryTabs categories={categories} onClick={handleTabClick} />
-      {currentCategory >= 0 ? <MenuArea menus={menuData} /> : <div className={styles.MainImage}></div>}
+      {currentCategory >= 0 ? (
+        <MenuArea animationClass={animationClass} menus={menuData} />
+      ) : (
+        <div className={styles.MainImage}></div>
+      )}
+      <Cart />
     </div>
   );
 }
-
-// const menus = [
-//   {
-//     커피: [
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "아메리카노",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "바닐라라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "카페모카",
-//         price: 4000,
-//       },
-//     ],
-//   },
-//   {
-//     라떼: [
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "아메리카노",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "바닐라라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "카페모카",
-//         price: 4000,
-//       },
-//     ],
-//   },
-//   {
-//     티: [
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "아메리카노",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "바닐라라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "카페모카",
-//         price: 4000,
-//       },
-//     ],
-//   },
-//   {
-//     쥬스: [
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "아메리카노",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "바닐라라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "카페모카",
-//         price: 4000,
-//       },
-//     ],
-//   },
-//   {
-//     디카페인: [
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "아메리카노",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "바닐라라떼",
-//         price: 4000,
-//       },
-//       {
-//         imgUrl: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mug-2031218_960_720.jpg",
-//         name: "카페모카",
-//         price: 4000,
-//       },
-//     ],
-//   },
-// ];
