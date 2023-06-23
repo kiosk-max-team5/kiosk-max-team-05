ebc758e91dad14e187a0a92ac2e3c7dc790918cf
bakhacode <bakhamitai@naver.com>
2023-06-23 10:44:25 +0900

    feat: 장바구니 UI 및 기능
    - 메뉴,전체취소 버튼, 결제하기 버튼 UI 구현
    - 메뉴의 X 닫기 버튼을 누르면 장바구니에서 메뉴가 사라진다.

---
+++ FE/src/components/cart/Cart.tsx
@@ -0,0 +1,61 @@
+import { useContext } from "react";
+import styles from "./Cart.module.css";
+import ModalContext, { CartMenus } from "../../ModalContext";
+
+export function Cart() {
+  const contextValue = useContext(ModalContext);
+  if (!contextValue) {
+    throw new Error("ModalContext is not provided");
+  }
+  const { isOpenCart, cartMenuList, setCartMenuList } = contextValue;
+
+  return isOpenCart ? (
+    <div className={styles.Cart}>
+      <div className={styles.MenuContainer}>
+        {cartMenuList?.map((menu, index) => (
+          <Menu key={index} menu={menu} index={index} cartMenuList={cartMenuList} setCartMenuList={setCartMenuList} />
+        ))}
+      </div>
+      <div className={styles.ButtonContainer}>
+        <div className={styles.CancelButton}>전체취소</div>
+        <div className={styles.PaymentButton}>결제하기</div>
+      </div>
+    </div>
+  ) : null;
+}
+
+function Menu({
+  menu,
+  index,
+  cartMenuList,
+  setCartMenuList,
+}: {
+  menu: CartMenus;
+  index: number;
+  cartMenuList: CartMenus[];
+  setCartMenuList: (menuList: CartMenus[]) => void;
+}) {
+  const handleCloseButton = () => {
+    handleRemoveFromCart(index);
+  };
+
+  const handleRemoveFromCart = (targetIndex: number) => {
+    if (!setCartMenuList || !cartMenuList) return; // 필요한 상태가 제공되지 않았다면 함수를 종료합니다.
+
+    const updatedCart = cartMenuList.filter((_, idx) => idx !== targetIndex);
+
+    setCartMenuList(updatedCart);
+  };
+
+  return (
+    <div className={styles.Menu}>
+      <div className={styles.MenuCount}>{menu.count}</div>
+      <img src={menu?.imgUrl} alt="menu" />
+      <div className={styles.MenuName}>{menu?.name}</div>
+      <div className={styles.MenuPrice}>{menu?.price}</div>
+      <div className={styles.MenuCloseButton} key={index} onClick={handleCloseButton}>
+        <span className={styles.MenuCloseButtonText}>X</span>
+      </div>
+    </div>
+  );
+}
