d58e2a22dbcfeb1310d6614c3e702c136f7f03e0
dokkisan <dokkisan.wang@gmail.com>
2023-06-26 20:11:14 +0900

    feat: 엔진엑스 경로 설정 추가

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -2,11 +2,13 @@
 
 S3_BUCKET="s3://kiosk-team5/deploy/web"
 PROJECT_ROOT="/home/ubuntu/web"
+NGINX_ROOT="/var/www/html"
 BUILD_FILE="react-build.zip"
 
+sudo rm -rf ${NGINX_ROOT}/*
 mkdir -p ${PROJECT_ROOT}
 aws s3 cp ${S3_BUCKET}/${BUILD_FILE} ${PROJECT_ROOT}/
-unzip ${PROJECT_ROOT}/${BUILD_FILE} -d ${PROJECT_ROOT}
+unzip ${PROJECT_ROOT}/${BUILD_FILE} -d ${NGINX_ROOT}
 rm ${PROJECT_ROOT}/${BUILD_FILE}
 
 sudo service nginx restart
