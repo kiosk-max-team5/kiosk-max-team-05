5ade5f7f83cd3a988843ff29d413db113507d593
dokkisan <dokkisan.wang@gmail.com>
2023-06-26 19:29:55 +0900

    fix: S3 버킷 경로 수정

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-S3_BUCKET="s3://kiosk-team5/deploy/app"
+S3_BUCKET="s3://kiosk-team5/deploy/web"
 PROJECT_ROOT="/home/ubuntu/web"
 BUILD_FILE="react-build.zip"
 
 mkdir -p ${PROJECT_ROOT}
 aws s3 cp ${S3_BUCKET}/${BUILD_FILE} .
-unzip react-build.zip -d ${PROJECT_ROOT}
-rm react-build.zip
+unzip ${BUILD_FILE}
+rm ${BUILD_FILE}
 
 sudo service nginx restart
